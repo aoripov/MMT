5599fa2d8412208998616a90ed4746d730b9f18a
Akbar Oripov <akbarzoripov@gmail.com>
2017-04-11 20:35:45 +0200

    Initiate Thesaurus

---
+++ src/mmt-thesaurus/src/info/kwarc/mmt/thesaurus/ServerPlugin.scala
@@ -0,0 +1,179 @@
+package info.kwarc.mmt.thesaurus
+
+import info.kwarc.mmt.api._
+import info.kwarc.mmt.api.web._
+import info.kwarc.mmt.api.frontend._
+import info.kwarc.mmt.api.documents._
+import info.kwarc.mmt.api.presentation._
+import info.kwarc.mmt.api.notations._
+import info.kwarc.mmt.api.backend._
+import info.kwarc.mmt.api.ontology._
+import info.kwarc.mmt.api.informal._
+import info.kwarc.mmt.stex._
+import symbols.{Constant}
+
+import utils._
+//import scala.util.parsing.json._
+import tiscaf._
+import scala.concurrent._
+
+case class ThesaurusError(text : String) extends Error(text)
+
+
+class ThesaurusPlugin extends ServerExtension("thesaurus") with Logger {
+  
+  override val logPrefix = "thesaurus"
+     /** Server */   
+  def apply(uriComps: List[String], query: String, body : Body, session: Session): HLet = {
+    lazy val json = body.asJSON match {
+      case j: JSONObject => j
+      case _ => throw ServerError("body must be json object")
+    }
+    
+    try {
+      uriComps match {
+        case "getNotations" :: _ => getNotations(json)
+        case "getDefinitions" :: _ => getDefinitions(json)
+        case "generateGlossary" :: _ => generateGlossary
+        case "foo" :: _ => foo
+        case _ => errorResponse("Invalid request: " + uriComps.mkString("/"), List(new ThesaurusError("Invalid Request" + uriComps)))
+       }
+    } catch {
+      case e : Error => 
+        log(e.shortMsg) 
+        errorResponse(e.shortMsg, List(e))
+      case e : Exception => 
+        errorResponse("Exception occured : " + e.getStackTrace(), List(e))
+    }
+  }
+  
+  def getNotations(params: JSONObject) = {
+      val spathS = params("spath").getOrElse(throw ServerError("No spath found")).toString
+      val languageO = params("language").map(_.toString)
+      val dimensionO = params("dimension").map(_.toString)
+      
+      val spath = Path.parse(spathS)
+      controller.get(spath) match {
+        case c : Constant =>
+          var notations = dimensionO match {
+            case None => c.notC.getAllNotations
+            case Some("parsing") => c.notC.parsingDim.notations.values.flatten
+            case Some("presentation") => c.notC.presentationDim.notations.values.flatten
+            case Some("verbalization") => c.notC.verbalizationDim.notations.values.flatten
+            case Some(s) => throw ServerError("Invalid notation dimension: '" + s  + "'. Expected parsing, presentation or verbalization")
+          }
+          
+          notations = languageO match {
+            case None => notations
+            case Some(lang) => notations.filter(_.scope.languages.contains(lang))
+          }
+          Server.JsonResponse(JSONArray(notations.map(n => JSONArray(toStringMarkers(n).map(s => JSONString(s)) : _*)).toSeq :_*))
+        case x => throw ServerError("Expected path pointing to constant, found :" + x.getClass())
+      }
+  }
+  
+  private def toStringMarkers(not : TextNotation) : List[String] = {
+   not.parsingMarkers flatMap {
+      case a : Arg => Some("_")
+      case a : SeqArg => Some("_...")
+      case a : ImplicitArg => None
+      case d : Delimiter => Some(d.text)
+      case v : Var => Some("_")
+      case _ => None
+    }
+  }
+  
+  private def getDefinitions(params: JSONObject) = {
+      val spathS = params("spath").getOrElse(throw ServerError("No spath found")).toString
+      val languageO = params("language").map(_.toString)
+      val spath = Path.parse(spathS)
+      var resultSet = controller.depstore.queryList(spath, ToObject(IRels.isDefinedBy))
+      resultSet = languageO match {
+        case None => resultSet
+        case Some(_) => resultSet.filter(p => sTeX.getLanguage(p) == languageO)
+      }
+      
+      //presenting
+      val pres = controller.extman.get(classOf[Presenter]).find(_.isApplicable("thesaurus")).getOrElse(throw ServerError("No presenter found"))
+      val resultNodes = resultSet flatMap {p => 
+        controller.get(p) match {
+          case s : StructuralElement =>
+            val rb = new presentation.StringBuilder
+            pres(s)(rb)
+            Some(rb.get)
+          case _ => None
+        }
+      }
+      Server.JsonResponse(JSONArray(resultNodes.map(s => JSONString(s)).toSeq :_*))
+  }
+  
+  private def generateGlossary = {
+    try {
+      val location = utils.File("/home/akbar/localmh/MathHub/glossary.html")
+//      val glossary = ThesaurusGenerator.generate(controller)
+      //utils.File.write(location, glossary)
+      print("GENERATING THESAURUS")
+      Server.TextResponse("Success")
+    } catch {
+      case e : Exception => Server.TextResponse(e.getMessage() + "\n" + e.getStackTrace.mkString("\n"))
+    }
+  }
+
+  private def foo = {
+    Server.JsonResponse(ThesaurusGenerator.generate(controller))
+  }
+  
+  //utils
+  private def errorResponse(text : String, errors : List[Throwable]) : HLet = {
+    JsonResponse("", s"MMT Error in Thesaurus extension: $text ", errors)
+  }
+  
+  private def JsonResponse(content : String, info : String, errors : List[Throwable]) : HLet = {
+    val response = new collection.mutable.HashMap[String, JSON]()
+    response("content") = JSONString(content)
+    if (errors == Nil) { //no errors
+      val status = new collection.mutable.HashMap[String, JSON]()
+      status("conversion") = JSONInt(0) //success
+      val messages = new collection.mutable.HashMap[String, JSON]()
+      if (info != "") {
+        val message = new collection.mutable.HashMap[String, JSON]()
+        message("type") = JSONString("Info")
+        message("shortMsg") = JSONString(info)
+        message("longMsg") = JSONString(info)
+        //no srcref
+        messages("0") = JSONObject(message.toSeq : _*)
+      }
+      status("messages") = JSONObject(messages.toSeq : _*)
+      response("status") = JSONObject(status.toSeq : _*)        
+    } else { //there are errors
+      val status = new collection.mutable.HashMap[String, JSON]()
+      if (content == "") {
+        status("conversion") = JSONInt(2) //failed with errors
+      } else {
+        status("conversion") = JSONInt(2) //success with errors
+      }
+      val messages = new collection.mutable.HashMap[String, JSON]()
+      errors.zipWithIndex foreach { p => 
+        val message = new collection.mutable.HashMap[String, JSON]()
+        p._1 match {
+          case se : SourceError =>
+            message("type") = JSONString("Fatal")
+            message("shortMsg") = JSONString(se.mainMessage)
+            message("longMsg") = JSONString(se.getStackTrace.mkString("\n"))
+            message("srcref") = JSONObject(List("from" -> JSONObject(List("line" -> JSONInt(se.ref.region.start.line), "col"-> JSONInt(se.ref.region.start.column)) : _*), 
+                                 "to" -> JSONObject(List("line" -> JSONInt(se.ref.region.end.line), "col" -> JSONInt(se.ref.region.end.column)) : _*)) : _*)
+          case e =>
+            message("type") = JSONString("Fatal")
+            message("shortMsg") = JSONString(e.getMessage)
+            message("longMsg") = JSONString(e.getStackTrace.mkString("\n"))
+            //no srcref :(
+          }
+          messages(p._2.toString) = JSONObject(message.toSeq : _*)
+      }
+      status("messages") = JSONObject(messages.toSeq : _*)
+      response("status") = JSONObject(status.toSeq : _*)
+    }
+      log("Sending Response: " + response)
+      Server.JsonResponse(JSONObject(response.toSeq : _*))     
+  }
+}
---
+++ src/mmt-thesaurus/src/info/kwarc/mmt/thesaurus/ThesaurusGenerator.scala
@@ -0,0 +1,227 @@
+package info.kwarc.mmt.thesaurus
+
+import info.kwarc.mmt.api._
+import modules._
+import symbols._
+import documents._
+import presentation._
+import frontend._
+import libraries._
+import objects._
+import utils._
+import informal._
+import archives._
+import notations._
+import scala.xml.{Node }
+import ontology._
+import info.kwarc.mmt.stex._
+
+object ThesaurusGenerator {
+
+  private var counter = 0
+  def getNewId : String = {
+    counter += 1
+    "gs_" + counter.toString
+  }
+  private var presenter: ThesaurusFormatter = null
+  private var controller: Controller = null
+  private var rh: StringBuilder = null
+
+  def generate(controller: Controller): JSON = {
+    this.controller = controller
+    this.presenter = controller.extman.get(classOf[Presenter], "thesaurus") match {
+      case Some(p: ThesaurusFormatter) => p
+      case _ => throw new Exception("Expected thesaurus formatter to be loaded")
+    }
+
+    val mpaths = controller.depstore.getInds(ontology.IsTheory).toList
+    val modules = mpaths flatMap { p =>
+      try {
+        controller.get(p) match {
+          case d : DeclaredTheory => Some(d)
+          case _ =>  None
+        }
+      } catch {
+        case e : Error => None
+        case e : Exception => None
+      }
+    }
+//    print(mpaths)
+    val verbs = modules collect {
+      case thy: DeclaredTheory =>
+        thy.getDeclarations collect {
+          case c: Constant if c.notC.verbalizationDim.isDefined =>
+            c.notC.verbalizationDim.notations.values.flatten.map(c.path -> _)
+        }
+    }
+//    println()
+//    print(verbs)
+//    println()
+//    print(verbs.flatten.flatten.toList.distinct)
+//    println()
+    val theories: List[(GlobalName, TextNotation)] = verbs.flatten.flatten.distinct
+    val head = theories.filter(p => {
+      println(p._1.name.toString)
+      true
+    })
+
+
+    val out = present(verbs.flatten.flatten.distinct)
+    JSONArray.fromList(out)
+  }
+
+  private def filter(verbs: Iterable[(GlobalName, TextNotation)]): Iterable[(GlobalName, TextNotation)] = {
+    verbs
+  }
+
+  // easy-to-use HTML markup
+  protected val htmlRh = utils.HTML(s => rh(s))
+  import htmlRh._
+
+  private def makeString(not : TextNotation) : String = {
+    val smks = not.markers map {
+      case d : Delimiter => d.text
+      case m => m.toString
+    }
+    smks.mkString(" ")
+  }
+
+  private def present(verbs: Iterable[(GlobalName, TextNotation)]): List[JSON] = {
+    val items = new collection.mutable.HashMap[String, List[(GlobalName, TextNotation)]]
+    verbs foreach {p => p._2.scope.languages.foreach { lang =>
+     if (lang != "") {
+       if (!items.contains(lang)) {
+         items(lang) = Nil
+       }
+       items(lang) ::= p
+     }
+    }}
+    def getCls(lang: String) = if (lang == "en") "active" else ""
+
+    val out = verbs map(x => this.present("en", x._1, x._2))
+//    div(attributes = List("id" -> "glossary")) {
+//      ul("nav nav-tabs") {
+//        items.foreach { p =>
+//          li(getCls(p._1)) {
+//            rh(<a data-target={ "#gtab_" + p._1 } class="gs_tab"> { p._1 } </a>)
+//          }
+//        }
+//      }
+//      div("tab-content") {
+//        items.foreach { p =>
+//          div(cls = ("tab-pane " + getCls(p._1)), attributes = List("id" -> ("gtab_" + p._1))) {
+//            ul("glossary") {
+//              val glossary = p._2.toList.sortWith((x,y) => makeString(x._2).toLowerCase() < makeString(y._2).toLowerCase())
+//              glossary.foreach(v => present(p._1, v._1, v._2))
+//            }
+//          }
+//        }
+//      }
+//    }
+    out.filter(x => x != null).toList
+  }
+  
+  private def getVerbalizations(path: MPath, lang: String) : List[TextNotation] = {
+    
+    //getPrimarySym(path : MPath) : Option[GlobalName]
+    val primarySyms = controller.depstore.getInds(IsPrimarySymbol)
+    val primarySymO = primarySyms.find { 
+      case p : GlobalName => p.module == path 
+      case _ => false
+    }
+    
+    //getVerbs(spath : Option[GlobalName], lang : String) : List[TextNotation]     
+    primarySymO.map(controller.get) match {
+      case Some(c:Constant) => c.notC.verbalizationDim.get(lang=Some(lang))
+      case _ => Nil
+    }
+  }
+
+  private def present(lang : String,  spath : GlobalName, not: TextNotation): JSON = {
+    val doc = spath.doc
+    val mod = spath.module.name
+    val name = spath.name
+    val constant = controller.library.getConstant(spath)
+    val alternatives = constant.notC.verbalizationDim.notations.values.flatten.flatMap(_.scope.languages.filter(_ != lang)).toSet
+
+    val notations = (constant.notC.parsingDim.notations.values.flatten ++ constant.notC.presentationDim.notations.values.flatten).map(n => spath -> n).toList.distinct
+
+    val defs = controller.depstore.queryList(spath, ToObject(IRels.isDefinedBy)) flatMap {
+          case p: Path =>
+            controller.getO(p) match {
+              case Some(fd: Constant) =>
+                val mpath = fd.home.toMPath
+                if (sTeX.getLanguage(mpath) contains lang) {
+                  Some(fd)
+                } else {
+                  None
+                }
+              case _ => None
+            }
+        }
+
+    val response = new collection.mutable.HashMap[String, JSON]()
+
+    val primarySym = controller.depstore.queryList(spath, HasType(IsPrimarySymbol))
+    val hypernyms = if (!primarySym.isEmpty) {
+      val mpath = spath.module 
+      controller.depstore.queryList(mpath, ToObject(IsHypernymOf)) flatMap {
+        case p: Path =>
+          controller.getO(p) match {
+            case Some(fd: Theory) =>
+              val mpath = fd.path
+              val verbs = getVerbalizations(mpath, lang)
+              if (verbs.isEmpty) {
+                None
+              } else {
+                Some(verbs)
+              }
+            case _ => None
+          }
+      }
+    } else Nil
+    
+    val hyponyms = if  (!primarySym.isEmpty) {
+      val mpath = spath.module 
+      controller.depstore.queryList(mpath, ToSubject(IsHypernymOf)) flatMap {
+        case p: Path =>
+          controller.getO(p) match {
+            case Some(fd: Theory) =>
+              val mpath = fd.path
+              val verbs = getVerbalizations(mpath, lang)
+              if (verbs.isEmpty) {
+                None
+              } else {
+                Some(verbs)
+              }
+            case _ => None
+          }
+      }
+    } else Nil
+    
+    print(defs.length)
+    
+    val synonyms = constant.notC.verbalizationDim.get(lang=Some(lang)).filter(_ != not)
+    
+    if (defs.isEmpty) {
+      return null
+    }
+
+    var sb = new StringBuilder
+    presenter.setRh(sb)
+    presenter.doNotationRendering(spath, not)
+
+    val out = new collection.mutable.HashMap[String, JSON]()
+    out("primary") = JSONString(sb.get)
+
+    sb = new StringBuilder
+    presenter.setRh(sb)
+    defs foreach { fd =>
+      presenter.apply(fd)(sb)
+    }
+
+    out("definition") = JSONString(sb.get)
+
+    JSONObject(out.toSeq : _*)
+  }
+}
